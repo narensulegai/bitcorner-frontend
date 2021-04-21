154228fe7b1ecef75036f51177ec819731e3b0be
Naren Sulegai <86naren@gmail.com>
2021-04-21 16:28:55 -0700

    Sell bitcoin UI

--- src/util/fetch/api/index.js
+++ src/util/fetch/api/index.js
@@ -6,6 +6,8 @@ export const getBankAccount = () => get('bankAccount');
 export const updateBankAccount = (d) => post('bankAccount', d);
 export const updateCustomer = (d) => post('customer', d);
 export const getCustomer = () => get('customer');
+export const getSellBitcoins = () => get('sellBitcoin');
+export const addSellBitcoins = (d) => post('sellBitcoin', d);
 
 export const fileUrl = (fileId) => {
   return `${apiUrl}/file/${fileId}`;
