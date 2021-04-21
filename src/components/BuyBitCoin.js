c1e90514f6cd674c857366900465bfa675dc8d2e
Naren Sulegai <86naren@gmail.com>
2021-04-21 16:53:54 -0700

    Buy bitcoin ui

--- src/components/BuyBitCoin.js
+++ src/components/BuyBitCoin.js
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { addBuyBitcoins, getBuyBitcoins } from '../util/fetch/api';
 
-const BuyBitCoin = () => {
+const BuyBitcoin = () => {
+  const [buyBitcoins, setBuyBitcoins] = useState([]);
+  const [marketOrder, setIsMarketPrice] = useState(true);
+  const amountRef = useRef(null);
+  const currencyRef = useRef(null);
+  const minimumPriceRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      setBuyBitcoins(await getBuyBitcoins());
+    })();
+  }, []);
+
+  const handleOnBuyBitcoin = async () => {
+    const amount = amountRef.current.value;
+    const currency = currencyRef.current.value;
+    let minPrice = null;
+    if (!marketOrder) {
+      minPrice = minimumPriceRef.current.value;
+    }
+    const d = {
+      marketOrder,
+      minPrice,
+      amount,
+      currency,
+    };
+    await addBuyBitcoins(d);
+    window.message('Added new buy order!');
+    amountRef.current.value = '';
+    currencyRef.current.value = '';
+    setIsMarketPrice(true);
+    setBuyBitcoins(await getBuyBitcoins());
+  };
+
+  const handleOnMarketPriceChange = () => {
+    setIsMarketPrice(!marketOrder);
+  };
+
   return (
-    <div>
-      Buy bitcoin
+    <div className="flex">
+
+      <div className="flex-full">
+        <div>
+          <h2>Sell Bitcoin</h2>
+        </div>
+        <div>
+          <div className="small-margin-top">
+            Amount <br /><input type="number" ref={amountRef} defaultValue="0" />
+          </div>
+          <div className="small-margin-top">
+            Currency <br /><input type="text" ref={currencyRef} defaultValue="USD" />
+          </div>
+          <div className="small-margin-top">
+            Market price &nbsp;<input type="checkbox" checked={marketOrder}
+              onChange={handleOnMarketPriceChange} />
+          </div>
+          {!marketOrder && (
+            <div className="small-margin-top">
+              Minimum price <br /><input type="number" ref={minimumPriceRef} defaultValue="0" />
+            </div>
+          )}
+          <div>
+            <button className="button" onClick={handleOnBuyBitcoin}>Buy bitcoin</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-full">
+        {buyBitcoins.length === 0
+          ? <div className="center">You have no bit coins to sell</div>
+          : (
+            <table className="table">
+              <thead>
+                <tr>
+                  <td>Currency</td>
+                  <td>Amount</td>
+                  <td>Price type</td>
+                  <td>Minimum price</td>
+                </tr>
+              </thead>
+              <tbody>
+                {buyBitcoins.map((b) => {
+                  return (
+                    <tr key={b.id}>
+                      <td>{b.currency}</td>
+                      <td>{b.amount}</td>
+                      <td>{b.marketOrder ? 'Market order' : 'Limit order'} </td>
+                      <td>{b.minPrice}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+      </div>
     </div>
   );
 };
 
-export default BuyBitCoin;
+export default BuyBitcoin;
--- src/util/fetch/api/index.js
+++ src/util/fetch/api/index.js
@@ -8,7 +8,8 @@ export const updateCustomer = (d) => post('customer', d);
 export const getCustomer = () => get('customer');
 export const getSellBitcoins = () => get('sellBitcoin');
 export const addSellBitcoins = (d) => post('sellBitcoin', d);
-
+export const getBuyBitcoins = () => get('buyBitcoin');
+export const addBuyBitcoins = (d) => post('buyBitcoin', d);
 export const fileUrl = (fileId) => {
   return `${apiUrl}/file/${fileId}`;
 };
